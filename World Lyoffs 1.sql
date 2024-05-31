544641143b7a03177c881d7dfa5769930f73c0a3
Shekhar Wadhawan <167020556+Shekhar2408@users.noreply.github.com>
2024-06-01 01:20:55 +0530

    Data Cleaning

--- World Lyoffs 1.sql
+++ World Lyoffs 1.sql
@@ -1,11 +1,20 @@
 -- Data Cleaning
 
+-- Creating a Database 
 Create Database world_layoffs;
 Use world_layoffs; 
 
 Select * from Layoffs;
 
--- 1 Delete any Duplicates 
+
+-- Now when we are data cleaning we usually follow a few steps
+-- 1. check for duplicates and remove any
+-- 2. standardize data and fix errors
+-- 3. Look at null values and see what 
+-- 4. remove any columns and rows that are not necessary - few ways
+
+
+-- First thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens.
 
 Create Table layoffs_staging
 Like layoffs;
@@ -16,6 +25,16 @@ layoffs;
 
 Select * From layoffs_staging;
 
+
+-- 1. Remove Duplicates
+
+-- First let's check for duplicates
+SELECT company, industry, total_laid_off,`date`,
+ROW_NUMBER() OVER (PARTITION BY company, industry, total_laid_off,`date`) AS row_num
+FROM world_layoffs.layoffs_staging;
+
+
+-- These are our real duplicates 
 With Duplicate_CTE AS
 (
 Select *,
@@ -27,6 +46,8 @@ Select *
 From Duplicate_CTE 
 Where Row_Num > 1;
 
+-- Creating a new Table which Contains only the unique dataset and not the duplicates
+	
 CREATE TABLE `layoffs_staging2` (
   `company` text,
   `location` text,
@@ -57,84 +78,156 @@ Where Row_num >1 ;
 Select * From layoffs_Staging2
 Where Row_num >1 ;
 
-Select * from layoffs_staging2; # A new Table with unique values
-
+Select * from layoffs_staging2; 
+	
 -- 2 Standardizing Data
-Select
-	Company, Trim(Company)
-From layoffs_staging2;   
 
-Update layoffs_staging2
-Set Company = Trim(Company);
+SELECT * 
+FROM layoffs_staging2;
 
-Select 
-	Distinct(Industry)
-From Layoffs_staging2;
+-- if we look at industry it looks like we have some null and empty rows, let's take a look at these
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+-- let's take a look at these
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company LIKE 'Bally%';
+-- nothing wrong here
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'airbnb%';
+
+-- It looks like airbnb is a travel, but this one just isn't populated.
+-- I'm sure it's the same for the others. What we can do is
+-- Write a query that if there is another row with the same company name, it will update it to the non-null industry values
+-- Makes it easy so if there were thousands we wouldn't have to manually check them all
+
+-- We should set the blanks to nulls since those are typically easier to work with
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+-- Now if we check those are all null
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+-- Now we need to populate those nulls if possible
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+-- and if we check it looks like Bally's was the only one without a populated row to populate this null values
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL 
+OR industry = ''
+ORDER BY industry;
+
+-- ---------------------------------------------------
+
+-- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
+
+-- now that's taken care of:
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY industry;
+
+-- --------------------------------------------------
+-- we also need to look at 
+
+SELECT *
+FROM layoffs_staging2;
+
+-- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY country;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country);
+
+-- now if we run this again it is fixed
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY country;
 
 
-Update Layoffs_staging2 
-Set Industry = 'crypto'
-Where Industry Like 'crypto%';
+-- Let's also fix the date columns:
+SELECT *
+FROM layoffs_staging2;
 
-Select
-	Distinct(Country), Trim(Trailing '.' From Country)
-From layoffs_staging2
-Order BY 1;
+-- we can use str to date to update this field
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 
-Update Layoffs_staging2 
-Set Country = Trim(Trailing '.' from Country)
-Where Country Like 'United Sates%';
-
-Select 
-	`date`
-From layoffs_staging2;    
-
-Update Layoffs_staging2
-Set `date` = Str_to_date(`date`, '%m/%d/%Y');
-
-Alter Table layoffs_staging2 
-Modify Column `date` Date;
-
-Select * 
-From layoffs_staging2
-Where Company = 'Airbnb';
-
-Select * 
-From layoffs_staging2
-Where Industry is NULL or Industry = '';
-
-Select t1.industry, t2.industry
-From layoffs_staging2 as t1
-Join layoffs_staging2 as t2 
-	On t1.company = t2.company
-Where (t1.industry is Null Or t1.industry = '')
-And t2.industry is not null;
+-- now we can convert the data type properly
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
 
 
-Update layoffs_staging2
-Set Industry = null
-Where Industry = '';
+SELECT *
+FROM layoffs_staging2;
 
 
-Update layoffs_staging2 as t1 
-Join layoffs_staging2 as t2 
-	On t1.company = t2.company
-Set t1.industry = t2.industry
-Where t1.industry is Null
-And t2.industry is not null;
 
 
-Select * 
-From Layoffs_staging2
-Where total_laid_off is null 
-And percentage_laid_off is null;
 
-Delete
-From Layoffs_staging2
-Where total_laid_off is null 
-And percentage_laid_off is null;
+-- 3. Look at Null Values
 
-Select * From Layoffs_staging2;
+-- the null values in total_laid_off, percentage_laid_off, and funds_raised_millions all look normal. I don't think I want to change that
+-- I like having them null because it makes it easier for calculations during the EDA phase
 
-Alter Table layoffs_staging2 
-Drop Column Row_Num;
+-- so there isn't anything I want to change with the null values
+
+
+
+
+-- 4. remove any columns and rows we need to
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+-- Delete Useless data we can't really use
+DELETE FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+
+SELECT * 
+FROM layoffs_staging2;
